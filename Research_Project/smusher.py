4b0b46a75c39f1e4a92bf86d66023c14d3e3c5b2
Jack Yang <jack73117311@gmail.com>
2023-05-05 19:03:37 -0400

    Update smusher.py

--- Research_Project/smusher.py
+++ Research_Project/smusher.py
@@ -1,17 +1,27 @@
+import os
 
-def remove_abstract_id_and_blank_lines(input_file, output_file):
+def remove_abstract_id_and_blank_lines(input_file, add_period=False):
     with open(input_file, 'r') as file:
         lines = file.readlines()
 
-    cleaned_lines = [line.replace('ABSTRACTID', '') for line in lines if line.strip() != '']
+    cleaned_lines = [line.replace('ABSTRACTID', '').rstrip() for line in lines if "ABSTRACTID" in line or line.strip() != '']
+    if add_period:
+        cleaned_lines = [line + '.' if line != '' else line for line in cleaned_lines]
+
+    input_file_basename, ext = os.path.splitext(input_file)
+    if "compressed" in input_file_basename:
+        output_file = input_file_basename.replace("raw_compressed", "compressed") + ext
+    elif "merged" in input_file_basename:
+        output_file = input_file_basename.replace("raw_merged", "merged") + ext
+    else:
+        output_file = input_file_basename + "_new" + ext
 
     with open(output_file, 'w') as file:
-        file.writelines(cleaned_lines)
+        file.writelines(line + '\n' for line in cleaned_lines)
 
 def main():
-    input_file = "Datasets/merged_dataset.txt"
-    output_file = "Datasets/merged_new.txt"
-    remove_abstract_id_and_blank_lines(input_file, output_file)
+    input_file = "Datasets/raw_compressed_dataset81.txt"
+    remove_abstract_id_and_blank_lines(input_file, add_period=True)
 
 if __name__ == "__main__":
     main()
